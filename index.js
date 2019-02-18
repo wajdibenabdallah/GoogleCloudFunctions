b7c4a40f1fd55e5a65e61bc9cc2d74ed55883ba6
wajdibenabdallah <wajdibenabdalla@gmail.com>
2019-02-18 14:14:20 +0100

    backupA

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+app.use(bodyParser.json()); // support json encoded bodies
+
 app.use(cors());
 
 const bucketName = 'sfeircvgenerator-storage';
